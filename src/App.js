50ff6f93cfee6def9a85c31ff4b9022c3df79220
Aniket-Bahirat <aniketbahirat2525@gmail.com>
2024-09-01 11:56:59 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import CitySelector from './CitySelector';
+import logo from './logo.svg';
+//import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+           <CitySelector/>
+    </div>
+  );
+}
+
+export default App;
